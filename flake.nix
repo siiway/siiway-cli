b5d92217f22ed76d67c7912ff0d7360a84861d07
RhenCloud <i@rhen.cloud>
2026-04-12 16:02:23 +0800

    add Nix cross-compilation support for Linux and Darwin

--- flake.nix
+++ flake.nix
@@ -7,42 +7,85 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs@{ self, flake-parts, systems, ... }:
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      systems,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import systems;
 
-      perSystem = { pkgs, system, ... }:
+      perSystem =
+        { pkgs, system, ... }:
         let
           goToolchain = if pkgs ? go_1_26 then pkgs.go_1_26 else pkgs.go;
 
-          package = pkgs.buildGoModule rec {
-            pname = "siiway-cli";
-            version =
-              if self ? shortRev then self.shortRev
-              else if self ? dirtyShortRev then self.dirtyShortRev
-              else "dev";
+          mkPackage =
+            {
+              targetGoos ? null,
+              targetGoarch ? null,
+            }:
+            pkgs.buildGoModule rec {
+              pname = "siiway-cli";
+              version =
+                if self ? shortRev then
+                  self.shortRev
+                else if self ? dirtyShortRev then
+                  self.dirtyShortRev
+                else
+                  "dev";
 
-            src = self;
-            vendorHash = "sha256-f247K347OFddpKF6nME/AqoDqwBLOJGfW/IpNRU6gds=";
+              src = self;
+              vendorHash = "sha256-f247K347OFddpKF6nME/AqoDqwBLOJGfW/IpNRU6gds=";
 
-            nativeBuildInputs = [ pkgs.makeWrapper ];
-            buildInputs = [ pkgs.git ];
+              nativeBuildInputs = [ pkgs.makeWrapper ];
+              buildInputs = [ pkgs.git ];
 
-            ldflags = [
-              "-s"
-              "-w"
-              "-X github.com/SiiWay/siiway-cli/cmd.Version=${version}"
-            ];
+              env = pkgs.lib.optionalAttrs (targetGoos != null && targetGoarch != null) {
+                GOOS = targetGoos;
+                GOARCH = targetGoarch;
+                CGO_ENABLED = "0";
+              };
 
-            postFixup = ''
-              wrapProgram $out/bin/siiway-cli \
-                --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.git ]}
-            '';
+              ldflags = [
+                "-s"
+                "-w"
+                "-X github.com/SiiWay/siiway-cli/cmd.Version=${version}"
+              ];
+
+              postFixup = ''
+                wrapProgram $out/bin/siiway-cli \
+                  --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.git ]}
+              '';
+            };
+
+          package = mkPackage { };
+          packageLinuxAmd64 = mkPackage {
+            targetGoos = "linux";
+            targetGoarch = "amd64";
+          };
+          packageLinuxArm64 = mkPackage {
+            targetGoos = "linux";
+            targetGoarch = "arm64";
+          };
+          packageDarwinAmd64 = mkPackage {
+            targetGoos = "darwin";
+            targetGoarch = "amd64";
+          };
+          packageDarwinArm64 = mkPackage {
+            targetGoos = "darwin";
+            targetGoarch = "arm64";
           };
         in
         {
           packages.default = package;
           packages.siiway-cli = package;
+          packages.siiway-cli-linux-amd64 = packageLinuxAmd64;
+          packages.siiway-cli-linux-arm64 = packageLinuxArm64;
+          packages.siiway-cli-darwin-amd64 = packageDarwinAmd64;
+          packages.siiway-cli-darwin-arm64 = packageDarwinArm64;
 
           apps.default = {
             type = "app";
